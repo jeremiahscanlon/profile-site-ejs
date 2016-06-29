2ff3dbdef039ef65dc1497397b62cb894ad34a30
jeremiahscanlon <jeremiah.j.scanlon@gmail.com>
2016-06-29 18:07:54 -0400

    continuing develompent

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,28 @@
 
 $(document).ready(function() {
 
+    console.log('loaded!');
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
+
+    // function handleShowrClick(arr){
+    //     $( '#'+arr ).removeClass( 'bounceOutUp' ).addClass( 'bounceInDown' ).show();
+    //     $( '#showr'+arr ).hide();
+    //     $( '#hidr'+arr ).show();
+    // }
+    //
+    // function handleHidrClick(arr){
+    //     $( '#'+arr ).removeClass( 'bounceInDown' ).addClass( 'bounceOutUp' );
+    //     $( '#hidr'+arr ).hide();
+    //     $( '#showr'+arr ).show();
+    // }
+    //
+    // var areas = ['Top', 'Bottom', 'Right', 'Left'];
+    //
+    // for (i=0; i < areas.length; i++) {
+    //     console.log('#showr'+areas[i]);
+    //     $( '#showr'+areas[i] ).click( handleShowrClick(areas[i]) );
+    //     $( '#hidr'+areas[i] ).click( handleHidrClick(areas[i]) );
+    // }
 
 });
